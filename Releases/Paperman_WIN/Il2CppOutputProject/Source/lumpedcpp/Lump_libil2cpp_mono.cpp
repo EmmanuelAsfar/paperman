6b0d750650e8abe893439934da22a2e05ed369b5
Emmanuel <emmanuel@asfar.net>
2021-04-15 10:05:36 +0200

    Removing UWP files

--- Releases/Paperman_WIN/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_mono.cpp
+++
@@ -1,7 +0,0 @@
-#include "il2cpp-config.h"
-#include "D:\Emmanuel\Developement\paperman\Releases\Paperman_WIN\Il2CppOutputProject\IL2CPP\libil2cpp\mono\MonoPosixHelper.cpp"
-#include "D:\Emmanuel\Developement\paperman\Releases\Paperman_WIN\Il2CppOutputProject\IL2CPP\libil2cpp\mono\ThreadPool\ThreadPoolMonitorThread.cpp"
-#include "D:\Emmanuel\Developement\paperman\Releases\Paperman_WIN\Il2CppOutputProject\IL2CPP\libil2cpp\mono\ThreadPool\ThreadPoolWorkerThread.cpp"
-#include "D:\Emmanuel\Developement\paperman\Releases\Paperman_WIN\Il2CppOutputProject\IL2CPP\libil2cpp\mono\ThreadPool\threadpool-ms-io-poll.cpp"
-#include "D:\Emmanuel\Developement\paperman\Releases\Paperman_WIN\Il2CppOutputProject\IL2CPP\libil2cpp\mono\ThreadPool\threadpool-ms-io.cpp"
-#include "D:\Emmanuel\Developement\paperman\Releases\Paperman_WIN\Il2CppOutputProject\IL2CPP\libil2cpp\mono\ThreadPool\threadpool-ms.cpp"
--- Releases/Paperman_WIN/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_vm-utils.cpp
+++
@@ -1,8 +0,0 @@
-#include "il2cpp-config.h"
-#include "D:\Emmanuel\Developement\paperman\Releases\Paperman_WIN\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\BlobReader.cpp"
-#include "D:\Emmanuel\Developement\paperman\Releases\Paperman_WIN\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\Debugger.cpp"
-#include "D:\Emmanuel\Developement\paperman\Releases\Paperman_WIN\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\NativeDelegateMethodCache.cpp"
-#include "D:\Emmanuel\Developement\paperman\Releases\Paperman_WIN\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\NativeSymbol.cpp"
-#include "D:\Emmanuel\Developement\paperman\Releases\Paperman_WIN\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\VmStringUtils.cpp"
-#include "D:\Emmanuel\Developement\paperman\Releases\Paperman_WIN\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\icalls\mscorlib\System.Threading\Interlocked.cpp"
-#include "D:\Emmanuel\Developement\paperman\Releases\Paperman_WIN\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\icalls\mscorlib\System\Math.cpp"
